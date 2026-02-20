59ca77ea1f50ad72c1ba1169641e775040c45752
JUDOKA104 <e.oriol@ecole-ipssi.net>
2026-02-20 15:39:21 +0100

    initial commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,50 @@
+// src/services/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, query, orderBy, limit, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAzVdUbw96jofAv5MesmWcO2VSvrjfZhyo",
+    authDomain: "opentdb-quiz.firebaseapp.com",
+    projectId: "opentdb-quiz",
+    storageBucket: "opentdb-quiz.firebasestorage.app",
+    messagingSenderId: "518589352129",
+    appId: "1:518589352129:web:f0ebe51af8625d533aac40"
+};
+
+// Initialisation de Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+// Sauvegarder un score
+export const saveScore = async (pseudo: string, score: number) => {
+    try {
+        const docRef = await addDoc(collection(db, "leaderboard"), {
+            pseudo: pseudo,
+            score: score,
+            date: new Date()
+        });
+        console.log("Score sauvegardé avec l'ID: ", docRef.id);
+    } catch (e) {
+        console.error("Erreur lors de l'ajout du score: ", e);
+    }
+};
+
+// Récupérer le Top 10
+export const getTop10Scores = async () => {
+    try {
+        const scoresRef = collection(db, "leaderboard");
+        const q = query(scoresRef, orderBy("score", "desc"), limit(10));
+
+        const querySnapshot = await getDocs(q);
+        const topScores: any[] = [];
+
+        querySnapshot.forEach((doc) => {
+            topScores.push({ id: doc.id, ...doc.data() });
+        });
+
+        return topScores;
+    } catch (e) {
+        console.error("Erreur lors de la récupération des scores: ", e);
+        return [];
+    }
+};
